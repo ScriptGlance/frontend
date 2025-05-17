be97f30664cda2a8450f106472b70172eb20d598
Nestorian <ananiev.nestor@ukr.net>
2025-05-18 01:55:29 +0300

    feat: split auth roles

--- src/App.tsx
+++ src/App.tsx
@@ -8,18 +8,25 @@ import NotFound from "./pages/notFound/NotFound.tsx";
 import ResetEmailSentPage from "./pages/forgotPassword/ResetEmailSentPage.tsx";
 import ForgotPasswordPage from "./pages/forgotPassword/ForgotPasswordPage.tsx";
 import ResetPasswordPage from "./pages/forgotPassword/ResetPasswordPage.tsx";
+import {Role} from "./types/role.ts";
 
 function App() {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<HomePage/>}/>
-                <Route path="/login" element={<LoginPage/>}/>
-                <Route path="/register" element={<RegisterPage/>}/>
-                <Route path="/forgot-password" element={<ForgotPasswordPage />} />
-                <Route path="/forgot-password/sent" element={<ResetEmailSentPage />} />
-                <Route path="/reset-password" element={<ResetPasswordPage />} />
 
+                <Route path="/login" element={<LoginPage role={Role.User}/>}/>
+                <Route path="/moderator/login" element={<LoginPage role={Role.Moderator}/>}/>
+                <Route path="/admin/login" element={<LoginPage role={Role.Admin}/>}/>
+
+                <Route path="/register" element={<RegisterPage/>}/>
+
+                <Route path="/forgot-password" element={<ForgotPasswordPage role={Role.User} />} />
+                <Route path="/moderator/forgot-password" element={<ForgotPasswordPage role={Role.Moderator} />} />
+                <Route path="/admin/forgot-password" element={<ForgotPasswordPage role={Role.Admin} />} />
+                <Route path="/forgot-password/sent" element={<ResetEmailSentPage/>}/>
+                <Route path="/reset-password" element={<ResetPasswordPage/>}/>
 
                 <Route path="/auth/login-success" element={<LoginSuccess/>}/>
                 <Route path="/auth/no-facebook-linked-emails-error" element={<FacebookEmailError/>}/>
