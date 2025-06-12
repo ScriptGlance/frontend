18ea53d23ee4c48b293eed11cc63be66b00a4226
Nestorian <ananiev.nestor@ukr.net>
2025-06-12 11:31:42 +0300

    fix: apk url

--- src/contstants.ts
+++ src/contstants.ts
@@ -1,4 +1,4 @@
 export const EMAIL_CONFIRMATION_TIME_SECONDS = 10 * 60;
 export const DEFAULT_ERROR_MESSAGE = 'Сталася помилка. Будь ласка, спробуйте ще раз.';
 export const PUNCTUATION_REGEX = /[.,!?;:"'"„“‘’`—–-]/g;
-export const APK_URL = "https://github.com/ScriptGlance/android/blob/main/app/ScriptGlance.apk"
+export const APK_URL = "https://github.com/ScriptGlance/android/releases/latest/download/ScriptGlance.apk"
